a77c8380680fe1b85ab8e102a9c13f6cddc78805
Arthur Damasceno <thuurdamasceno@gmail.com>
2025-01-29 09:44:12 -0300

    29JAN25 - Aula 250 AS - Upload de img para o Storage.

--- ImageUploadActivity.kt
+++ ImageUploadActivity.kt
@@ -1,5 +1,6 @@
 package com.arttt95.aulafirebase
 
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -8,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.arttt95.aulafirebase.databinding.ActivityImageUploadBinding
+import com.google.firebase.storage.FirebaseStorage
 
 class ImageUploadActivity : AppCompatActivity() {
 
@@ -15,11 +17,20 @@ class ImageUploadActivity : AppCompatActivity() {
         ActivityImageUploadBinding.inflate(layoutInflater)
     }
 
+    private val armazenamento by lazy {
+        FirebaseStorage.getInstance()
+    }
+
+    private var uriImagemSelecionada: Uri? = null // Uri -> android.net
+
     private val abrirGaleria = registerForActivityResult(
         ActivityResultContracts.GetContent()
     ) { uri ->
         if(uri != null) {
             binding.imgSelecionada.setImageURI(uri)
+
+            uriImagemSelecionada = uri
+
             Toast.makeText(this, "Img selecionada!", Toast.LENGTH_SHORT).show()
         } else {
             Toast.makeText(this, "Nenhuma img selecionada!", Toast.LENGTH_SHORT).show()
@@ -39,6 +50,42 @@ class ImageUploadActivity : AppCompatActivity() {
         binding.btnGaleria.setOnClickListener {
 
             abrirGaleria.launch("image/*") // Mime Type
+
+        }
+
+        binding.btnUpload.setOnClickListener {
+
+            uploadGaleria()
+
+        }
+
+    }
+
+    private fun uploadGaleria() {
+
+        /*
+        -> videos
+        -> fotos
+            -> viagens
+                -> foto1.jpg
+        */
+
+        if(uriImagemSelecionada != null) {
+
+            armazenamento
+                .getReference("fotos")
+                .child("viagens")
+                .child("foto.jpg")
+                .putFile( uriImagemSelecionada!! )
+                .addOnSuccessListener { task ->
+                    Toast.makeText(this,"Sucesso ao realizar upload da img.", Toast.LENGTH_SHORT).show()
+                    task.metadata?.reference?.downloadUrl
+                        ?.addOnSuccessListener { uriFirebase ->
+                            Toast.makeText(this, uriFirebase.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                }.addOnFailureListener { exception ->
+                    Toast.makeText(this,"Erro ao realizar upload da img. Code: ${exception.message}", Toast.LENGTH_SHORT).show()
+                }
 
         }
 
